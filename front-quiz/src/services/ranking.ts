87fce85659826a31e659bb70fff8034b735d3cfd
mariaEdCaldas <127269057+mariaEdCaldas@users.noreply.github.com>
2023-11-24 19:47:14 -0400

    Add função de acesso ao banco para o rank

---
+++ front-quiz/src/services/ranking.ts
@@ -0,0 +1,34 @@
+import { collection, getDocs, getDoc, addDoc, doc, query, where, updateDoc} from "firebase/firestore";
+import { db } from "./firebaseConfig";
+
+  export interface RankingData {
+    user: string;
+    ranking: string; 
+    pontuacao: number;
+    
+  }
+  
+  export async function setUserRank(email: string, pontuacaoNova: number) {
+    const q = query(collection(db, 'ranking'), where('user', '==', email));
+  
+    if (q) {
+      const querySnapshot = await getDocs(q);
+  
+      querySnapshot.docs.map((doc) =>
+        updateDoc(doc.ref, {
+          pontuacao: doc.data().pontuacao + pontuacaoNova,
+        })
+      );
+    } else {
+      addDoc(collection(db, 'ranking'), {
+        user: email,
+        pontuacao: pontuacaoNova,
+      });
+    }
+  }
+  
+  export async function getRank(): Promise<RankingData[]> {
+    const querySnapshot = await getDocs(collection(db, 'ranking'));
+    return querySnapshot.docs.map((doc) => doc.data() as RankingData);
+  }
+  
