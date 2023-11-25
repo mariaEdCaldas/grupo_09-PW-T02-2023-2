8f33125bd0e7c14ab7be3cee8bc053573e9bf13c
diogomene <diogodelimamenezes15@gmail.com>
2023-11-25 17:25:45 -0400

    New start to firebase

--- firebase-functions/index.js
+++
@@ -1,5 +0,0 @@
-import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
-import {logger} from "firebase-functions";
-import {onRequest} from "firebase-functions/v2/https";
-import {onDocumentCreated} from "firebase-functions/v2/firestore";
