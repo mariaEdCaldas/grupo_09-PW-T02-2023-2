ca20fd782aec6455e343fa374c9756672b0e32cd
João P <joao.karpinski@ufms.br>
2023-11-25 13:26:17 -0400

    update

--- front-quiz/src/pages/home/Home.tsx
+++ front-quiz/src/pages/home/Home.tsx
@@ -1,12 +1,41 @@
 import "./Home.scss";
-import SelectInput from "../../components/form/inputs/selectInput/SelectInput";
+import SelectInput, { InputOption } from "../../components/form/inputs/selectInput/SelectInput";
 import GameCard from "../../components/gameCard/GameCard";
 import { DifficultyEnum } from "../../models/Difficulty";
 import IsNotLoggedRedirecter from "../../components/isNotLoggedRedirecter/IsNotLoggedRedirecter";
+import { Quiz } from "../../models/Quiz";
+import { useEffect, useState } from "react";
+import { getAllQuiz } from "../../services/quiz";
+import { getAllCategoria } from "../../services/categoria";
+import {auth} from "../../services/firebaseConfig"
 
 export default function Home() {
-    const categoriesOptions = ["Geografia"];
-    const difficultyOptions = Object.values(DifficultyEnum)
+    const [categoriesOptions, setCategoriesOptions] = useState<InputOption[]>([]);
+
+    const getCategories = async () => {
+        const categoriasRes : InputOption[] = (await getAllCategoria()).map((categoria) => {
+            return {
+                value: categoria.nome,
+                label: categoria.nome,
+            };
+        })
+        setCategoriesOptions(categoriasRes);
+    }
+    const difficultyOptions = Object.values(DifficultyEnum).map(
+        (difficulty) => {
+            return {
+                value: difficulty,
+                label: difficulty,
+            };
+        }
+    )
+    const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+    useEffect(() => {
+        getCategories();
+        getAllQuiz().then((quizzes) => {
+            setQuizzes(quizzes);
+        }  );
+    }, []);
     return (
         <>
             <IsNotLoggedRedirecter/>
@@ -20,10 +49,16 @@ export default function Home() {
                     </div>
                 </div>
                 <div className="game-list-container">
-                    <GameCard id="vida-semi-arido" category="Geografia" difficulty="Moleza" name="A vida no semi-árido"/>
-                    <GameCard id="planicies" category="Geografia" difficulty="Desafiador" name="Sobre as planícies"/>
-                    <GameCard id="irlanda-do-norte" category="Geografia" difficulty="Impossível" name="Irlanda do norte"/>
-
+                    {quizzes.map((quiz, idx) => (
+                        <GameCard
+                            key={idx}
+                            id={quiz.id||"0"}
+                            name={quiz.nome}
+                            difficulty={quiz.dificuldade as DifficultyEnum}
+                            category={quiz.categoria.nome}
+                        />
+                    ))}
+                
                 </div>
             </div>
         </>
