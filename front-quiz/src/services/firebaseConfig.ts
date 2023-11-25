8b52f9f073ab010d9fc5bb8a963b9da5b47f49cf
João P <joao.karpinski@ufms.br>
2023-11-25 13:27:37 -0400

    firebase

--- front-quiz/src/services/firebaseConfig.ts
+++ front-quiz/src/services/firebaseConfig.ts
@@ -2,6 +2,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 
@@ -27,6 +28,6 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-
-export {app, auth}
+export {app, auth, db}
