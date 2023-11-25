b4e5e6e7653b7839f35cd7db96b75b60795c969a
diogomene <diogodelimamenezes15@gmail.com>
2023-11-25 15:32:56 -0400

    initializing firebase-side application code

---
+++ firebase-functions/index.js
@@ -0,0 +1,5 @@
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
+import {logger} from "firebase-functions";
+import {onRequest} from "firebase-functions/v2/https";
+import {onDocumentCreated} from "firebase-functions/v2/firestore";
